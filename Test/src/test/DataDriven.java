f08812aaf62c30ac1c20acd2a178a2063bfce6a4
Siddu <sgouti3@gmail.com>
2019-07-12 22:40:10 +0530

    updated Test

--- Test/src/test/DataDriven.java
+++ Test/src/test/DataDriven.java
@@ -14,6 +14,8 @@ import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.util.NumberToTextConverter;
 
+import com.relevantcodes.extentreports.LogStatus;
+import com.relevantcodes.extentreports.model.Log;
 
 public class DataDriven {
 
@@ -31,54 +33,118 @@ public class DataDriven {
 		Wrkbk.close();
 	}
 
-	public static ArrayList<String> ExecutableTStCntrl(String Shtnme) {
+	public static void ExecutableTStCntrl(String Shtnme) {
 		try {
-			Sheet TestCntrlSheet = Sheetnme(Shtnme);
-			ArrayList<String> TestCntrlHdrs = Headersvlue(TestCntrlSheet);
-			// int TestIDRow=Hdrcnt(TestCntrlSheet,"TC Id");
+			
+			Sheet TestCntrlSheet = Sheetnme(Shtnme, "");
+			ArrayList<String> ExecutableTstCases = ExecutableTestIDs(TestCntrlSheet, GlobalV.TestCaseID, GlobalV.Run);
+			for(int i=0;i<ExecutableTstCases.size();i++)
+			{
+			int TCROwNmbr=RowNumbr(TestCntrlSheet,GlobalV.TestCaseID,ExecutableTstCases.get(i));
+			String Browser= CellData(TestCntrlSheet,TCROwNmbr,GlobalV.Browsers);
+			System.out.println("TestIDNames  "+ ExecutableTstCases.get(i));
+			ReportLog.RemoveInstance();
+			ReportLog.GetInstance(ExecutableTstCases.get(i),"");
+			TestScript(GlobalV.TestCaseFilePath,ExecutableTstCases.get(i));
+			//Use Testscript method
+			AfterSuit();
+			}
+			
+			
 
-			ArrayList<String> ExecutableTstCase = TstCntrlRwVue(TestCntrlSheet, "TC Id", "Status");
-			System.out.println(ExecutableTstCase + "" + TestCntrlHdrs);
-			TestScript(ExecutableTstCase, TestCntrlSheet);
-			return ExecutableTstCase;
 		} catch (IOException e) {
 			System.out.println("Error is :-" + e);
 		}
-		return null;
 
 	}
 
-	public static void TestScript(ArrayList<String> ExecutbleScript, Sheet ShtNme) throws IOException {
-		String TcID = ExecutbleScript.get(1);
-		System.out.println("Test Id is" + TcID);
-		Sheet TestCntrlSheet = Sheetnme("TestCase");
-		ArrayList<String> TestCntrlHdrs = Headersvlue(TestCntrlSheet);
-		// int TestIDRow=Hdrcnt(TestCntrlHdrs,"TC Id");
-		ArrayList<String> ExecutableTstCase = TstCntrlRwVue(ShtNme, "TC Id", "Status");
-
-		System.out.println(ExecutableTstCase + "" + TestCntrlHdrs);
-		ExcutbleRows(TestCntrlSheet, "TC Id", TcID);
-
-		System.out.println("Custimezed row" + RowValue(TestCntrlSheet, TestCntrlHdrs.get(0), "TC02"));
-	}
-
-	// Taking sheetname from excel
-	public static Sheet Sheetnme(String Sheetname) throws IOException {
-		FileInputStream file = new FileInputStream("Sid.xls");
-		HSSFWorkbook Wrkbk = new HSSFWorkbook(file);
-		Sheet sheetName = null;
-		int TotlShts = Wrkbk.getNumberOfSheets();
-		int i = 0;
-		while (i < TotlShts) {
-			if (Wrkbk.getSheetAt(i).getSheetName().equalsIgnoreCase(Sheetname)) {
-				sheetName = Wrkbk.getSheetAt(i);
-				System.out.println("SheetName is :-  " + sheetName.getSheetName());
-				break;
-			}
-			i++;
+	public static void TestScript(String ShtNme,String TestCaseID) throws IOException {
+		Sheet TestCntrlSheet = Sheetnme(ShtNme, "");
+		ArrayList<String> ExecutableFnNme = new ArrayList<String>();
+		ArrayList<Integer> RowsNumbrs = ExcutbleRows(TestCntrlSheet, GlobalV.TestCaseID,TestCaseID, GlobalV.Execute);
+		System.out.println("Script Rows ID  "+RowsNumbrs);
+		
+		for(int i=0;i<RowsNumbrs.size();i++)
+		{
+			ExecutableFnNme.add(CellData(TestCntrlSheet,RowsNumbrs.get(i),GlobalV.FunctionName));
+			String FunctionNme=CellData(TestCntrlSheet,RowsNumbrs.get(i),GlobalV.FunctionName);
+			Keyword(GlobalV.KeywordLibraryFilePath,FunctionNme);
 		}
-		Wrkbk.close();
-		return sheetName;
+	System.out.println("FunctionNames"+ExecutableFnNme);
+	
+	
+	}
+	
+	public static void Keyword(String ShtNme,String FunctNme) throws IOException
+	{
+		Sheet KeywrdSheet=Sheetnme(ShtNme, "");
+		ArrayList<Integer> RowsNumbrs = ExcutbleRows(KeywrdSheet, GlobalV.FunctionName,FunctNme, GlobalV.Execute);
+		System.out.println("Keywordscripts No.-"+RowsNumbrs);
+		
+		for(int i=0;i<RowsNumbrs.size();i++)
+		{
+			String Description=CellData(KeywrdSheet,RowsNumbrs.get(i),GlobalV.StepDescription);
+			String ActionKyrd=CellData(KeywrdSheet,RowsNumbrs.get(i),GlobalV.ActionOrKeyword);
+			String LogicalNme=CellData(KeywrdSheet,RowsNumbrs.get(i),GlobalV.ObjectLogicalName);
+			ReportLog.GetInstance().log(LogStatus.INFO,ActionKyrd,Description);
+			
+		System.out.println((i+1)+" )  "+Description+" :: "+ActionKyrd+" :: " + LogicalNme);
+		}
+		
+	
+	}
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	public static void AfterSuit() {
+		ReportLog.Instance().endTest(ReportLog.GetInstance());
+		ReportLog.Instance().flush();
+	}
+
+	
+	// Taking sheetname from excel
+	public static Sheet Sheetnme(String Sheetname, String FilePath) {
+		try {
+			FileInputStream file = new FileInputStream("Sid.xls");
+			HSSFWorkbook Wrkbk = new HSSFWorkbook(file);
+			Sheet sheetName = null;
+			int TotlShts = Wrkbk.getNumberOfSheets();
+			int i = 0;
+			while (i < TotlShts) {
+				if (Wrkbk.getSheetAt(i).getSheetName().equalsIgnoreCase(Sheetname)) {
+					sheetName = Wrkbk.getSheetAt(i);
+					break;
+				}
+				i++;
+			}
+			Wrkbk.close();
+			return sheetName;
+		} catch (IOException e) {
+			System.out.println("Failed get sheetNaame from given filepath: " + e);
+			return null;
+		}
 	}
 
 	// Taking all headersName
@@ -94,16 +160,15 @@ public class DataDriven {
 				HdrNme.add(CV);
 			} else {
 				HdrNme.add(NumberToTextConverter.toText(clv.getNumericCellValue()));
-
 			}
 
 		}
-		System.out.println(HdrNme);
+	//	System.out.println(HdrNme);
 		return HdrNme;
 
 	}
 
-//Taking selected HeaderClm Number
+	// Taking selected HeaderClm Number
 	public static int Hdrcnt(Sheet Sheetname, String Hdrnme) throws IOException {
 		ArrayList<String> HeadrNme = Headersvlue(Sheetname);
 		int HdrcntN = 0;
@@ -119,21 +184,18 @@ public class DataDriven {
 
 	}
 
-//Taking All Executable Testcases
-	public static ArrayList<String> TstCntrlRwVue(Sheet Sheetname, String Clmnme, String Status) throws IOException {
-		// Sheet Shtnme=Sheetnme(ShtNme);
-
-		Iterator<Row> Row = Sheetname.iterator();
+	// Taking All Executable Testcases
+	public static ArrayList<String> ExecutableTestIDs(Sheet SheetName, String TCIDHdr, String ExecuteHdr) throws IOException {
+	
+		Iterator<Row> Row = SheetName.iterator();
 		ArrayList<String> RowVlue = new ArrayList<String>();
-		int Hdr = Hdrcnt(Sheetname, Clmnme);
-		int StatusNmr = Hdrcnt(Sheetname, Status);
-		System.out.println("Status Header Number:-" + StatusNmr);
+		int Hdr = Hdrcnt(SheetName, TCIDHdr);
+		int StatusNmr = Hdrcnt(SheetName, ExecuteHdr);
+	//	System.out.println("Status Header Number:-" + StatusNmr);
 		while (Row.hasNext()) {
 			Row Row1 = Row.next();
-			// System.out.println(Row1.getCell(Hdr).getStringCellValue());
-
 			Demo: {
-				while (Row1.getCell(Hdr).getStringCellValue().equalsIgnoreCase(Clmnme)) {
+				while (Row1.getCell(Hdr).getStringCellValue().equalsIgnoreCase(TCIDHdr)) {
 					break Demo;
 				}
 
@@ -154,11 +216,11 @@ public class DataDriven {
 	}
 
 	// Taking All selected Rows Number for Executing
-	public static ArrayList<Integer> ExcutbleRows(Sheet Shtnme, String Clmnme, String CellValue) throws IOException {
+	public static ArrayList<Integer> ExcutbleRows(Sheet Shtnme, String Clmnme, String CellValue,String Excute) throws IOException {
 		// Sheet ActiveSheet = Sheetnme(Shtnme);
 		Iterator<Row> Row = Shtnme.iterator();
 		int Hdr = Hdrcnt(Shtnme, Clmnme);
-		int StatusHDr = Hdrcnt(Shtnme, "Status");
+		int StatusHDr = Hdrcnt(Shtnme, Excute);
 		ArrayList<Integer> RowNumbr = new ArrayList<>();
 		while (Row.hasNext()) {
 			Row Row1 = Row.next();
@@ -170,12 +232,28 @@ public class DataDriven {
 
 			}
 		}
-		System.out.println(RowNumbr);
-		for (int a = 0; a < RowNumbr.size(); a++)
-			System.out.println(RowValues(Shtnme, RowNumbr.get(a)));
+	
+	return RowNumbr;
 
+	}
+
+	// Taking only one Row Number for executing
+	public static int RowNumbr(Sheet Shtnme, String Clmnme, String CellValue) throws IOException {
+		Iterator<Row> Row = Shtnme.iterator();
+		int Hdr = Hdrcnt(Shtnme, Clmnme);
+
+		int RowNumbr = 0;
+		while (Row.hasNext()) {
+			Row Row1 = Row.next();
+
+			if (Row1.getCell(Hdr).toString().equalsIgnoreCase(CellValue)) {
+				RowNumbr = Row1.getRowNum();
+				break;
+
+			}
+
+		}
 		return RowNumbr;
-
 	}
 
 	// Taking All values from Selected Row
@@ -195,23 +273,15 @@ public class DataDriven {
 		return RowValue;
 
 	}
+	
+	public static String CellData(Sheet shtNme, int RownNumbr,String ColumnNme) throws IOException {
+		
+		int HeadrsNmr = Hdrcnt(shtNme,ColumnNme);
+		Row Rowvlue=shtNme.getRow(RownNumbr);
+		String Datavlue=Rowvlue.getCell(HeadrsNmr).getStringCellValue();
+		return Datavlue;
+		
 
-	public static int RowValue(Sheet Shtnme, String Clmnme, String CellValue) throws IOException {
-		Iterator<Row> Row = Shtnme.iterator();
-		int Hdr = Hdrcnt(Shtnme, Clmnme);
-
-		int RowNumbr = 0;
-		while (Row.hasNext()) {
-			Row Row1 = Row.next();
-
-			if (Row1.getCell(Hdr).toString().equalsIgnoreCase(CellValue)) {
-				RowNumbr = Row1.getRowNum();
-				break;
-
-			}
-
-		}
-		return RowNumbr;
 	}
 
 }
